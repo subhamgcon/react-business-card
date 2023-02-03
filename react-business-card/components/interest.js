4d24f9ba863ffd0f253de0c98f344716c4bfd9b4
Subham Ghosh <74487802+subhamgcon@users.noreply.github.com>
2023-02-04 02:24:51 +0530

    Delete react-business-card directory

--- react-business-card/components/interest.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-export default function Interest(){
-    return(
-        <div>
-        <h1>Interest</h1>
-        <h5>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</h5>
-        </div>
-    )
-}

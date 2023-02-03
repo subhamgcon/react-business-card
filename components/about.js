bda2a30ced3ce16a0bb400cbc1f491844d58caf2
Subham Ghosh <74487802+subhamgcon@users.noreply.github.com>
2023-02-04 02:25:47 +0530

    Add files via upload

---
+++ components/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+export default function About(){
+    return(
+        <div className = "about">
+        <h1>About</h1>
+        <h5>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</h5>
+        </div>
+    )
+}
